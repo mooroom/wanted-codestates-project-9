e1b48b4ad36e24928cb25b6d1a35b9d84489ff7f
mooroom <61869246+mooroom@users.noreply.github.com>
2022-03-23 15:56:45 +0900

    Chore: 초기 세팅
    - 필수 패키지 설치
    - proxy 설정
    - axios config 설정

---
+++ src/GlobalStyles.ts
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  /* reset */
+  *{margin:0;padding:0;font:inherit;color:inherit;}
+  *,:after, :before {box-sizing:border-box;flex-shrink:0;}
+  :root {-webkit-tap-highlight-color:transparent;-webkit-text-size-adjust:100%;text-size-adjust:100%;cursor:default;line-height:1.5;overflow-wrap:break-word;-moz-tab-size:4;tab-size:4}
+  html {scroll-behavior: smooth;}
+  html, body {width: 100%; height:100%;}
+  img, picture, video, canvas, svg {display: block;max-width:100%;};
+  button {background:none;border:0;cursor:pointer;}
+  a {text-decoration:none}
+  table {border-collapse:collapse;border-spacing:0}
+
+  * { font-family: 'Spoqa Han Sans Neo', 'sans-serif'; }
+`;
+
+export default GlobalStyles;
